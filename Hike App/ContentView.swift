35304e1beab998adc81291ff7063da6533cd487e
Resn4R <purview-joust.0x@icloud.com>
2023-12-13 15:07:11 +0000

    Initial Commit

---
+++ Hike App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hike App
+//
+//  Created by Vito Borghi on 13/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike App/Hike_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_AppApp.swift
+//  Hike App
+//
+//  Created by Vito Borghi on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hike_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
